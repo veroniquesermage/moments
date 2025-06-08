f5ec8f927f01db2a6119f2734de5bd6bb1f9cbbb
Véronique Sermage <ransomstoddart@gmail.com>
2025-06-08 22:00:22 +0200

    ajout de la duplication d'une idée de cadeau

--- backend/app/services/ideas_service.py
+++ backend/app/services/ideas_service.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -6,7 +7,7 @@ from app.core.enum import GiftStatusEnum
 from app.core.logger import logger
 from app.models import GiftIdeas, Gift, User, UserGroup
 from app.schemas import UserSchema
-from app.schemas.gift import GiftIdeasResponse
+from app.schemas.gift import GiftIdeasResponse, GiftCreate
 from app.schemas.gift import GiftPublicResponse
 from app.schemas.gift import GiftIdeasSchema
 from app.schemas.gift import GiftIdeaCreate
@@ -55,7 +56,10 @@ class GiftIdeasService:
         logger.info(f"Création d'une idée de cadeau pour l'utilisateur {current_user}")
 
         if current_user.id == gift_idea.gift.destinataire_id:
-            raise ValueError("L'utilisateur ne peut pas proposer une idée de cadeau pour lui-même.")
+            raise HTTPException(
+                status_code=400,
+                detail="❌ L'utilisateur ne peut pas proposer une idée de cadeau pour lui-même."
+            )
 
         idea = GiftIdeas(
             proposee_par_id=current_user.id,
@@ -77,3 +81,67 @@ class GiftIdeasService:
             gift=GiftPublicResponse.model_validate(gift),
             gift_idea=GiftIdeasSchema.model_validate(idea)
         )
+
+    @staticmethod
+    async def change_visibility( db: AsyncSession,
+                                 current_user: User,
+                                 ideaId: int,
+                                 visibility: bool) :
+        logger.info(f"Changement de visibilité de l'idée {ideaId} pour l'utilisateur {current_user.id}")
+
+        existing = (await db.execute(select(GiftIdeas)
+                         .where(GiftIdeas.id == ideaId))).scalars().first()
+
+        if not existing :
+            raise HTTPException(
+                status_code=400,
+                detail=f"❌ L'idée de cadeau avec l\'ID {ideaId} n\'existe pas."
+            )
+
+        if existing.proposee_par_id != current_user.id:
+            raise HTTPException(
+                status_code=403,
+                detail="❌ Vous n'êtes pas autorisé à modifier la visibilité de cette idée."
+            )
+
+        existing.visibilite = visibility
+
+        await db.commit()
+        await db.refresh(existing)
+
+
+    @staticmethod
+    async def duplicate_gift_idea(db: AsyncSession,
+                                    current_user: User,
+                                    ideaId: int,
+                                    new_dest_id: int) -> GiftIdeasResponse:
+
+            logger.info(f"Dupliquer l'idée de cadeau {ideaId} pour l'utilisateur {current_user.id}")
+
+            existing: Gift = (await db.execute(select(Gift)
+                             .where(Gift.gift_idea_id == ideaId)
+                             .options(selectinload(Gift.gift_idea)))).scalars().first()
+
+            if not existing:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"❌ L'idée de cadeau avec l'ID {ideaId} n'existe pas."
+                )
+
+            idea = existing.gift_idea
+            if idea.proposee_par_id != current_user.id:
+                raise HTTPException(
+                    status_code=403,
+                    detail="❌ Vous n'êtes pas autorisé à dupliquer cette idée."
+                )
+
+            gift_data = GiftCreate.model_validate(existing, from_attributes=True).model_dump()
+            gift_data["destinataire_id"] = new_dest_id
+
+            gift_idea_create = GiftIdeaCreate(
+                gift=GiftCreate(**gift_data),
+                visibilite=existing.gift_idea.visibilite
+            )
+
+            return await GiftIdeasService.create_gift_idea(db, current_user, gift_idea_create)
+
