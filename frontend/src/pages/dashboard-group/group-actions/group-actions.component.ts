932fa9f19433f4fb9fdbff6687306c23c36b6d08
Véronique Sermage <ransomstoddart@gmail.com>
2025-06-23 19:21:20 +0200

    gestion groupe par l'admin

---
+++ frontend/src/pages/dashboard-group/group-actions/group-actions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-actions',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-actions.component.html',
+  styleUrl: './group-actions.component.scss'
+})
+export class GroupActionsComponent {
+
+}
---
+++ frontend/src/pages/dashboard-group/group-roles/group-roles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-roles.component.html',
+  styleUrl: './group-roles.component.scss'
+})
+export class GroupRolesComponent {
+
+}
