9d4c45630671c163be1ab9cc8f507028593a58cc
Véronique Sermage <ransomstoddart@gmail.com>
2025-06-27 09:57:17 +0200

    suppression du groupe

--- backend/app/routes/user_group_route.py
+++ backend/app/routes/user_group_route.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.dependencies.current_user import get_current_user
 from app.models import User
+from app.schemas import UserDisplaySchema
 from app.services import UserGroupService
 
 router = APIRouter(prefix="/utilisateur-groupe", tags=["Utilisateur & Groupe"])
@@ -32,3 +33,12 @@ async def update_nickname(
 
     await UserGroupService.update_nickname(db, current_user, groupId, nickname)
     return
+
+@router.patch("/{groupId}/update", response_model=list[UserDisplaySchema])
+async def update_roles(
+        groupId: int,
+        payload: list[UserDisplaySchema],
+        db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user)
+) -> list[UserDisplaySchema] :
+    return await UserGroupService.update_role(db, groupId, current_user, payload)
