e91336147a5d9b079986723d80d3e9707a7799a3
Véronique Sermage <ransomstoddart@gmail.com>
2025-06-28 09:49:39 +0200

    suppression du groupe - 2

--- backend/app/routes/user_group_route.py
+++ backend/app/routes/user_group_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, Depends, HTTPException, Response
+from fastapi import APIRouter, Body, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
