35df7d89e2a46a2876e0bf44870af9fde0b09d9d
Véronique Sermage <ransomstoddart@gmail.com>
2025-07-11 14:27:13 +0200

    correction retour refresh

--- backend/app/routes/auth_route.py
+++ backend/app/routes/auth_route.py
@@ -8,16 +8,16 @@ from app.services.auth.auth_service import AuthService
 
 router = APIRouter(prefix="/api/auth", tags=["Authentification"])
 
-@router.post("/google", response_model=None)
+@router.post("/google", status_code=200)
 async def authenticate_with_google(
     request: GoogleAuthRequest,
     db: AsyncSession = Depends(get_db)
 ) -> JSONResponse:
     return await AuthService.authenticate_google_user(request, db)
 
-@router.post("/refresh",  status_code=200)
+@router.post("/refresh", status_code=200)
 async def refresh_token(
         request: Request,
         db: AsyncSession = Depends(get_db)
 ):
-    await AuthService.refresh_access_token(db, request)
+    return await AuthService.refresh_access_token(db, request)
