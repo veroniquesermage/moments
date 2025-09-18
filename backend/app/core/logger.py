1038c13b4271128eb8f9a769c1375ed6ce24b50b
Véronique Sermage <ransomstoddart@gmail.com>
2025-09-18 21:34:10 +0200

    modification des invitations pour ajout du guide utilisateur

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 # Crée un dossier 'logs' si besoin
 os.makedirs("logs", exist_ok=True)
 
-logger = logging.getLogger("liste2wish")
+logger = logging.getLogger("moments")
 logger.setLevel(logging.DEBUG)  # Pour permettre tous les niveaux
 
 # Format commun
@@ -20,7 +20,7 @@ console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 
 # Handler fichier avec rotation (par défaut INFO et plus)
-file_handler = RotatingFileHandler("logs/app.log", maxBytes=1_000_000, backupCount=3)
+file_handler = RotatingFileHandler("logs/app.log", maxBytes=5_000_000, backupCount=5)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 
